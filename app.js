107bd97c2621078fcfe212c15a16e147094faa86
Wes Reid <bwreid@gmail.com>
2017-03-28 12:07:54 -0700

    new route plus port defined by env var

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-/*
-  Start by going to the expressjs.com website and find the "Hello World" example. Get your server started using it!
-*/
-
-// What type of thing is `require`?
 var express = require('express')
-// What type of thing is `express`?
 var app = express()
+var port = process.env.PORT || 3000
 
-// What type of thing is `app`? What about `.get`?
 app.get('/', function (req, res) {
-  // `res.send` is similar to something we learned yesterday. What is it?
+  // What status code gets sent by default?
   res.send('Hello World!')
 })
 
-// Change the port to a new one and restart your server.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/ping', function (req, res) {
+  res.send('pong!')
 })
 
-// NEXT STEPS: 
-// - Add a new `get` route called '/ping' that responds with 'pong!'
-// - Change the port to be 3000 by default or respond to an env variable
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
+})
+
+// NEXT STEPS:
+// - Let's name all of the functions we're using as arguments and separate them
+// - Respond with a different status code on the '/ping' route
